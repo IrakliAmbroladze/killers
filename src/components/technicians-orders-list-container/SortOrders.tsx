6fdd3b1d2dc66ac858e0e24dcbb1208afbfb45fd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-05-16 13:02:28 +0400

    feat(middleware): add technicians restrictian to sales

---
+++ src/components/technicians-orders-list-container/SortOrders.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import TechniciansOrdersList from "./technicians-orders-list";
+import { Sheets_Invoice } from "@/types/invoices";
+import { orderListTitles } from "./utils/orderListTitles";
+import { filterByStatus } from "./utils/filterByStatus";
+
+interface SortOrdersProps {
+  orders: Sheets_Invoice[];
+}
+
+const SortOrders = ({ orders }: SortOrdersProps) => {
+  const toBePlannedOrders = filterByStatus(orders, "empty");
+  const plannedOrders = filterByStatus(orders, "planned");
+  const doneOrders = filterByStatus(orders, "done");
+  return (
+    <div className="flex gap-5">
+      <TechniciansOrdersList
+        orders={toBePlannedOrders}
+        title={orderListTitles[0]}
+      />
+      <TechniciansOrdersList
+        orders={plannedOrders}
+        title={orderListTitles[1]}
+      />
+      <TechniciansOrdersList orders={doneOrders} title={orderListTitles[2]} />
+    </div>
+  );
+};
+
+export default SortOrders;
--- src/components/technicians-orders-list-container/technicians-orders-list-container.tsx
+++ src/components/technicians-orders-list-container/technicians-orders-list-container.tsx
@@ -1,24 +1,12 @@
 "use client";
 import React from "react";
 import { useOrders } from "@/context/orders-context";
-import TechniciansOrdersList from "./technicians-orders-list";
+import SortOrders from "./SortOrders";
 
 const TechniciansOrdersListCotainer = () => {
   const orders = useOrders();
-  const toBePlannedOrders =
-    orders && orders.filter((order) => order.delivery_date === "");
-  const doneOrders =
-    orders && orders.filter((order) => order.delivery_date !== "");
-  return (
-    <div className="flex gap-5">
-      <TechniciansOrdersList
-        orders={toBePlannedOrders}
-        title="მიღებული შეკვეთები"
-      />
-      <TechniciansOrdersList orders={null} title="დაგეგმილი შეკვეთები" />
-      <TechniciansOrdersList orders={doneOrders} title="შესრულებული" />
-    </div>
-  );
+
+  return orders && <SortOrders orders={orders} />;
 };
 
 export default TechniciansOrdersListCotainer;
