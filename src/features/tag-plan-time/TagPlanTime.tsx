743a5848389c3354a9739cf3c872fa637a2ba4cf
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-05-29 00:56:09 +0400

    make plan_time approvable

--- src/features/tag-plan-time/TagPlanTime.tsx
+++ src/features/tag-plan-time/TagPlanTime.tsx
@@ -1,38 +1,42 @@
 "use client";
 
-import React, { JSX } from "react";
+import React, { useState, JSX } from "react";
 import { useOrders } from "@/hooks/useOrders";
 import { findOrder } from "@/utils/findOrder";
 import DatePicker from "react-datepicker";
 import { updateOrderInDB } from "@/utils/updateOrderInDB";
+import { FaCheck } from "react-icons/fa6";
 
 const TagPlanTime = ({ order_id }: { order_id: string }): JSX.Element => {
   const { orders, updateOrder } = useOrders();
   const order = findOrder(orders, order_id);
 
-  const selectedDate = order?.plan_time ? new Date(order.plan_time) : null;
+  const currentDate = order?.plan_time ? new Date(order.plan_time) : null;
+  const [selectedDate, setSelectedDate] = useState<Date | null>(currentDate);
 
-  const handleChange = (date: Date | null) => {
-    if (order) {
-      const updatedOrder = {
-        ...order,
-        plan_time: date ? date.toISOString() : "",
-      };
-      updateOrder(updatedOrder);
-      updateOrderInDB(updatedOrder);
-    }
+  const handleApprove = () => {
+    if (!order) return;
+
+    const updatedOrder = {
+      ...order,
+      plan_time: selectedDate ? selectedDate.toISOString() : "",
+    };
+
+    updateOrder(updatedOrder);
+    updateOrderInDB(updatedOrder);
   };
 
   if (!order) return <div>Order not found</div>;
 
   return (
-    <div className="flex gap-2 max-w-xs">
-      <label htmlFor={`plan-time-${order_id}`} className="text-s font-medium">
+    <div className="flex gap-0.5 max-w-xs">
+      <label htmlFor={`plan-time-${order_id}`} className="text-sm font-medium">
         დრო:
       </label>
+
       <DatePicker
         selected={selectedDate}
-        onChange={handleChange}
+        onChange={(date) => setSelectedDate(date)}
         showTimeSelect
         timeFormat="HH:mm"
         timeIntervals={15}
@@ -40,6 +44,13 @@ const TagPlanTime = ({ order_id }: { order_id: string }): JSX.Element => {
         placeholderText="აირჩიე დღე და საათი"
         isClearable
       />
+
+      <button
+        onClick={handleApprove}
+        className=" bg-blue-600 text-white rounded hover:bg-green-700"
+      >
+        <FaCheck />
+      </button>
     </div>
   );
 };
