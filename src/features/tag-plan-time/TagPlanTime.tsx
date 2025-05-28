2987cb6094ffcd07bfa8e3e2e076835c5eb00d87
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-05-28 20:18:16 +0400

    feat(tag-plan-time): final version before refactor

--- src/features/tag-plan-time/TagPlanTime.tsx
+++ src/features/tag-plan-time/TagPlanTime.tsx
@@ -1,35 +1,33 @@
 "use client";
 
-import React, { JSX, useState } from "react";
+import React, { JSX } from "react";
 import { useOrders } from "@/hooks/useOrders";
 import { findOrder } from "@/utils/findOrder";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 
 const TagPlanTime = ({ order_id }: { order_id: string }): JSX.Element => {
   const { orders, updateOrder } = useOrders();
   const order = findOrder(orders, order_id);
 
-  const [selectedDate, setSelectedDate] = useState<Date | null>(
-    order?.plan_time ? new Date(order.plan_time) : null
-  );
+  const selectedDate = order?.plan_time ? new Date(order.plan_time) : null;
 
   const handleChange = (date: Date | null) => {
-    setSelectedDate(date);
     if (order) {
       const updatedOrder = {
         ...order,
         plan_time: date ? date.toISOString() : "",
       };
       updateOrder(updatedOrder);
-      console.log(updatedOrder);
     }
   };
 
-  return (
-    <div className="flex flex-col gap-2 max-w-xs">
-      <div className="font-medium">{order?.customer}</div>
+  if (!order) return <div>Order not found</div>;
 
+  return (
+    <div className="flex gap-2 max-w-xs">
+      <label htmlFor={`plan-time-${order_id}`} className="text-s font-medium">
+        დრო:
+      </label>
       <DatePicker
         selected={selectedDate}
         onChange={handleChange}
@@ -37,7 +35,7 @@ const TagPlanTime = ({ order_id }: { order_id: string }): JSX.Element => {
         timeFormat="HH:mm"
         timeIntervals={15}
         dateFormat="yyyy-MM-dd HH:mm"
-        placeholderText="Plan time"
+        placeholderText="აირჩიე დღე და საათი"
         isClearable
       />
     </div>
