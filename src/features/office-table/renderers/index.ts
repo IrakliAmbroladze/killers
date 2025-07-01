d36471ffee479999504b596f3838e708a3ee24c6
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-07-01 20:01:37 +0400

    feat(ag-grid): make doc field clickable

--- src/features/office-table/renderers/index.ts
+++ src/features/office-table/renderers/index.ts
@@ -2,3 +2,4 @@ export { saveButton } from "./saveButton";
 export { createDeleteButton } from "./deleteButton";
 export { createCopyButton } from "./copyButton";
 export { createViewButton } from "./viewButton";
+export { documentLinkRenderer } from "./linkRenderer";
