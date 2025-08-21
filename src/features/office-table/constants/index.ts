8e632f96311513b44b413bae91641697b5bf94d1
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-08-21 21:25:38 +0400

    refac: remove unused features

--- src/features/office-table/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export { myTheme } from "./myTheme";
-export { getColumnDefs } from "./columnDefs";
-export { paginationConfig } from "./pagination";
-export { defaultColumnDefs } from "./defaultColDef";
-export { rowSelection } from "./rowSelection";
