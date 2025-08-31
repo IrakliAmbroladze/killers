b21d285f64e065e2109899f8942f09c6223449ed
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-08-31 16:48:41 +0400

    refac: remove unneccessary use of getAuthenticatedUser

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,11 +1,8 @@
 import { NextResponse } from "next/server";
 import { getCommentsQuantities } from "@/lib/getCommentsQuantities";
-import { getAuthenticatedUser } from "@/lib/auth/getAuthenticatedUser";
 
 export async function GET() {
   try {
-    const user = await getAuthenticatedUser();
-    console.log("Authenticated request from:", user?.email);
     const commentsQuantities = await getCommentsQuantities();
     return NextResponse.json(commentsQuantities);
   } catch (error) {
