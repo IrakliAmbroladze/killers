1ad5c53186213186d0b965b5fa2e37e198688059
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-28 00:19:15 +0400

    fix(orders-list): when empty string and not null in db, order was thought to be planned

--- src/features/to-do-list/utils/mapOrdersToTasks.ts
+++ src/features/to-do-list/utils/mapOrdersToTasks.ts
@@ -2,12 +2,7 @@ import { OrderExtended } from "@/types";
 
 export const mapOrdersToTasks = (orders: OrderExtended[]) => {
   return orders.map((order) => {
-    const status_id =
-      order.delivery_date === null && order.plan_time === null
-        ? 0
-        : order.delivery_date === null && order.plan_time !== null
-        ? 1
-        : 2;
+    const status_id = !order.delivery_date ? (!order.plan_time ? 0 : 1) : 2;
 
     return { status_id, ...order };
   });
