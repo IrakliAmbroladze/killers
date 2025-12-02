eede512c40633280df4c5feda3efd4acfa92bb44
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-02 18:42:48 +0400

    feat(customersTable): add optimistic update on contractor checkbox

--- src/components/atoms/ContractorStatus.tsx
+++ src/components/atoms/ContractorStatus.tsx
@@ -5,12 +5,27 @@ import { Customer } from "@/types";
 import { useState } from "react";
 
 export const ContractorStatus = ({ customer }: { customer: Customer }) => {
+  const [isUpdating, setIsUpdating] = useState(false);
   const [checkStatus, setCheckStatus] = useState<boolean>(
     customer.contractor ?? false,
   );
   const handleClick = async (testCustomer: Customer) => {
+    const previousValue = checkStatus;
     setCheckStatus((prev) => !prev);
-    await editCustomer(testCustomer);
+    setIsUpdating(true);
+    try {
+      const response = await editCustomer(testCustomer);
+      if (response.status !== "OK") {
+        throw new Error("Update failed");
+      }
+    } catch (error) {
+      setCheckStatus(previousValue);
+
+      console.error("Update failed:", error);
+      alert("❌ დაფიქსირდა შეცდომა");
+    } finally {
+      setIsUpdating(false);
+    }
   };
   return (
     <label className="flex w-full justify-around" htmlFor={customer.id}>
@@ -18,6 +33,7 @@ export const ContractorStatus = ({ customer }: { customer: Customer }) => {
         id={customer.id}
         type="checkbox"
         checked={checkStatus}
+        disabled={isUpdating}
         onChange={(e) =>
           handleClick({ ...customer, contractor: e.target.checked })
         }
