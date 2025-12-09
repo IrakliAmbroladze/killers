4d160c762b80b50a549500cbafafc2131f45eaf4
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-10 01:14:54 +0400

    feat(acceptance): create main table

--- src/app/protected/orders/acceptance-document/[order_id]/page.tsx
+++ src/app/protected/orders/acceptance-document/[order_id]/page.tsx
@@ -16,9 +16,22 @@ export default async function AcceptanceDocument({
     "დეზინფექცია",
     "ქვეწარმავლების პრევენცია",
   ];
+  const tableData = [
+    { pest: "ბუზი", chemic: "Killzone მღრღ. ფირფიტა", doze: "-" },
+    { pest: "ქინქლა", chemic: "Killzone მწერის ფირფიტა", doze: "-" },
+    { pest: "ტარაკანი", chemic: "BROMOBLEU", doze: "-" },
+    { pest: "ჭიანჭველა", chemic: "RATIMOR", doze: "-" },
+    { pest: "რწყილი", chemic: "Cipex10e", doze: "" },
+    { pest: "ბაღლინჯო", chemic: "RAPTOR GEL", doze: "-" },
+    { pest: "თაგვი", chemic: "BLEU DELTA", doze: "" },
+    { pest: "ვირთხა", chemic: "AGITA", doze: "" },
+    { pest: "ქვეწარმავალი", chemic: "SURFANIOS PREMIUM", doze: "" },
+    { pest: "ბაქტერია", chemic: "REPTIL NATURAL STOP", doze: "-" },
+    { pest: "ბზიკი", chemic: "ALFADOM", doze: "" },
+  ];
 
   return (
-    <div className="flex justify-center items-center flex-col gap-5 px-2.5">
+    <div className="flex justify-center items-center flex-col gap-5 px-2.5 ">
       <LogoWhiteOnBlue />
       <h1>მიღება-ჩაბარების აქტი</h1>
       <input type="date" defaultValue={getTodaysYYYY_MM_DDString()} />
@@ -36,6 +49,122 @@ export default async function AcceptanceDocument({
             <label htmlFor={service}>{service}</label>
           </div>
         ))}
+      </div>
+      <div className="w-full overflow-auto">
+        <table className="border border-collapse min-w-[300px] mx-auto">
+          <tr>
+            <th colSpan={7}>
+              ტერიტორიაზე ჩატარებული სამუშაოები და სამიზნე მავნებლები:
+            </th>
+          </tr>
+          <tr>
+            <th rowSpan={3}>მავნებელი</th>
+            <th colSpan={3} rowSpan={2}>
+              გატარებული ღონისძიება
+            </th>
+            <th colSpan={3}>გამოყენებული საშუალებები</th>
+          </tr>
+          <tr>
+            <th>დასახელება</th>
+            <th>დოზირება</th>
+            <th>გახარჯული</th>
+          </tr>
+          <tr>
+            <th>მონიტორი</th>
+            <th>სპრეი</th>
+            <th>გელი</th>
+            <td>
+              <label className="flex gap-2.5 justify-between">
+                <input type="checkbox" />
+                მხოხავი მავნებლის ფირფიტა
+              </label>
+            </td>
+            <td>-</td>
+            <td></td>
+          </tr>
+          {tableData.map((td) => (
+            <tr key={td.pest}>
+              <td className="min-w-[250px] shrink-0">
+                <label className="flex gap-2.5 justify-between">
+                  <input type="checkbox" />
+                  {td.pest}
+                </label>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="min-w-[300px] shrink-0">
+                <label className="flex gap-2.5 justify-between">
+                  <input type="checkbox" />
+                  {td.chemic}
+                </label>
+              </td>
+              <td>{td.doze}</td>
+              <td></td>
+            </tr>
+          ))}
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <th colSpan={3}>მიწოდებული ინვენტარი</th>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <th>დასახელება</th>
+            <th>ფასი</th>
+            <th>რაოდენობა</th>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
       </div>
     </div>
   );
