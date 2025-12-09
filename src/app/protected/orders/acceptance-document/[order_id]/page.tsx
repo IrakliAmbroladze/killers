5d0678551d1ee943f7134f5eeace03350ca3874f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-09 15:14:00 +0400

    feat: create fetchOrderById function

--- src/app/protected/orders/acceptance-document/[order_id]/page.tsx
+++ src/app/protected/orders/acceptance-document/[order_id]/page.tsx
@@ -1,8 +1,12 @@
+import { fetchOrderById } from "@/lib/supabase/fetchOrderById";
+
 export default async function AcceptanceDocument({
   params,
 }: {
   params: Promise<{ order_id: string }>;
 }) {
   const { order_id } = await params;
+  const order = await fetchOrderById(order_id);
+  console.log("order is: ", order);
   return <div>My order id is: {order_id}</div>;
 }
