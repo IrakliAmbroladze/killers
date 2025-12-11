0f0bcf0c0602226c39d185bfcfb6189c141a90d9
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-11 15:34:40 +0400

    feat: create canvas ref

--- src/components/Signature/SignatureCanvas.tsx
+++ src/components/Signature/SignatureCanvas.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { useRef } from "react";
+
 export const SignatureCanvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   return <canvas className="border rounded-md w-full h-56 bg-white" />;
 };
