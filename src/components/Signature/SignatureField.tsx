ddd69e5a8cc402ab62a25c47662f13a42912f3af
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-12 12:04:59 +0400

    feat(Acceptance): add dignature clear button

--- src/components/Signature/SignatureField.tsx
+++ src/components/Signature/SignatureField.tsx
@@ -1,5 +1,19 @@
 import { SignatureCanvas } from "./SignatureCanvas";
 
 export const SignatureField = () => {
-  return <SignatureCanvas />;
+  return (
+    <>
+      <SignatureCanvas />
+
+      <button
+        className="px-4 py-2 bg-gray-200 rounded text-[#000] cursor-pointer hover:scale-105 active:scale-95"
+        /*   onClick={() => {
+            sigRef.current?.clear();
+            setHasSignature(false);
+          }}*/
+      >
+        Clear
+      </button>
+    </>
+  );
 };
