7c0d8703cabf9ef769ff0643edcb35a83ca13fa8
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-26 19:12:17 +0400

    refac(pdf-api): remove form flatten

--- src/pdf/layout/checkboxes.ts
+++ src/pdf/layout/checkboxes.ts
@@ -1,4 +1,3 @@
-import { PDFPage } from "pdf-lib";
 import { MARGIN_X, PAGE_WIDTH } from "../constants/pdfPageDimensions";
 import { Services } from "../types/SanitaryServices";
 import { Cursor } from "../types/Cursor";
@@ -6,13 +5,11 @@ import { PDFDrawer } from "../classes/PDFDrawer";
 
 type drawServicesCheckBoxesProps = {
   services: Services[];
-  page: PDFPage;
   cursor: Cursor;
   drawer: PDFDrawer;
 };
 export const drawServicesCheckBoxes = ({
   services,
-  page,
   cursor,
   drawer,
 }: drawServicesCheckBoxesProps) => {
@@ -23,16 +20,6 @@ export const drawServicesCheckBoxes = ({
     const xPos = MARGIN_X + 100 + col * colWidth;
     const yPos = cursor.y - row * 20;
     drawer.drawCheckbox(xPos, yPos, service.checked, 8);
-
-    /* service.field.addToPage(page, {
-      x: xPos,
-      y: yPos,
-      width: 10,
-      height: 10,
-    });
-    if (service.checked) {
-      service.field.check();
-    }*/
     drawer.drawText(service.label, xPos + 15, yPos, { size: 10 });
   });
 
--- src/pdf/types/SanitaryServices.ts
+++ src/pdf/types/SanitaryServices.ts
@@ -1,13 +1,10 @@
 import { AcceptanceFormData } from "@/types";
-import { PDFCheckBox, PDFForm } from "pdf-lib";
 
 export type SanitaryServices = {
-  form: PDFForm;
   formData: AcceptanceFormData;
 };
 
 export type Services = {
   label: string;
   checked: boolean;
-  field: PDFCheckBox;
 };
