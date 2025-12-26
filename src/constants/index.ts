c2c05bbb53dea577005dd2bc653fb34aae959500
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-26 11:14:30 +0400

    refac: extract pestTableData constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export { monthNamesInGeoArray } from "./calendar/monthNamesInGeoArray";
 export { weekDayNamesInGeoArray } from "./calendar/weekDayNamesInGeoArray";
 export { currentMonth } from "./calendar/currentMonth";
 export { currentYear } from "./calendar/currentYear";
+export { pestTableData } from "./acceptance";
