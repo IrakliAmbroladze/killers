2ea59bef6f910af0c7442ecb02e03123e28945ec
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-30 17:59:29 +0400

    refac: create useAcceptanceForm hook for SOLID principle

--- src/app/protected/orders/acceptance-document/[order_id]/AcceptanceDocument.tsx
+++ src/app/protected/orders/acceptance-document/[order_id]/AcceptanceDocument.tsx
@@ -4,115 +4,26 @@ import { getTodaysYYYY_MM_DDString } from "@/utils/calendar/getTodaysString";
 import DoneAreas from "./DoneAreas";
 import ProcedureTime from "./ProcedureTime";
 import AcceptanceSignature from "./AcceptanceSignature";
-import { AcceptanceFormData, OrderExtended } from "@/types";
-import { useState } from "react";
+import { OrderExtended } from "@/types";
 import { acceptanceFormData } from "@/constants";
 import { Table } from "./Table";
-import { TableCell } from "@/pdf/types/Table";
+import { useAcceptanceForm } from "@/hooks";
 
 export default function AcceptanceDocument({
   order,
 }: {
   order: OrderExtended;
 }) {
-  const [formData, setFormData] = useState<AcceptanceFormData>(
-    acceptanceFormData(order),
-  );
-
-  const pestRows: TableCell[][] = formData.pests.map((pest) => [
-    { type: "text", text: pest.name },
-    {
-      type: "checkbox",
-      checked: pest.monitor,
-      pestName: pest.name,
-      field: "monitor",
-    },
-    {
-      type: "checkbox",
-      checked: pest.spray,
-      pestName: pest.name,
-      field: "spray",
-    },
-    {
-      type: "checkbox",
-      checked: pest.gel,
-      pestName: pest.name,
-      field: "gel",
-    },
-  ]);
-  const materialRows: TableCell[][] = formData.products.map((material) => [
-    { type: "text", text: material.name },
-    { type: "text", text: material.dosage },
-    { type: "inputText", materialName: material.name, value: material.used },
-  ]);
-
-  const inventoryRows: TableCell[][] = formData.inventory.map(
-    (item, rowIndex) => [
-      {
-        type: "inventoryInputText",
-        rowIndex,
-        field: "name",
-        value: item.name,
-      },
-      {
-        type: "inventoryInputText",
-        rowIndex,
-        field: "price",
-        value: item.price,
-      },
-      {
-        type: "inventoryInputText",
-        rowIndex,
-        field: "quantity",
-        value: item.quantity,
-      },
-    ],
-  );
-
-  const handlePestEventChange = (
-    pestName: string,
-    field: "monitor" | "spray" | "gel",
-    checked: boolean,
-  ) => {
-    setFormData((prev) => ({
-      ...prev,
-      pests: prev.pests.map((pest) =>
-        pest.name === pestName ? { ...pest, [field]: checked } : pest,
-      ),
-    }));
-  };
-  const handleMaterialEventChange = (materialName: string, value: string) => {
-    setFormData((prev) => ({
-      ...prev,
-      products: prev.products.map((product) =>
-        product.name === materialName ? { ...product, used: value } : product,
-      ),
-    }));
-  };
-  const handleSoldInventoryChange = (
-    rowIndex: number,
-    field: "name" | "price" | "quantity",
-    value: string,
-  ) => {
-    setFormData((prev) => ({
-      ...prev,
-      inventory: prev.inventory.map((item, index) =>
-        index === rowIndex ? { ...item, [field]: value } : item,
-      ),
-    }));
-  };
-
-  const handleServicesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, checked } = e.target;
-
-    setFormData((prev) => ({
-      ...prev,
-      services: {
-        ...prev.services,
-        [name]: checked,
-      },
-    }));
-  };
+  const {
+    formData,
+    handleServicesChange,
+    handleSoldInventoryChange,
+    handlePestEventChange,
+    handleMaterialEventChange,
+    pestRows,
+    materialRows,
+    inventoryRows,
+  } = useAcceptanceForm(acceptanceFormData(order));
 
   return (
     <div className="flex justify-center items-center flex-col gap-5 px-2.5 ">
