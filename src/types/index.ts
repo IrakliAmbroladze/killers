7fa2ae2da85e7cb02d88e094dc14674254360871
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-05 20:49:11 +0400

    refac(HandleServicesChange): move type in table.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,5 @@
 export type { AcceptanceFormData } from "./documents/AcceptanceFormData";
-export type { HandleServicesChange } from "./documents/HandleServicesChange";
-export type { UiTableCell } from "./documents/table";
+export type { UiTableCell, HandleServicesChange } from "./documents/table";
 export type { Order, OrderExtended } from "./Order";
 export type { Customer } from "./Customer";
 export * from "./calendar";
