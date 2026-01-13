ed0a984d0e624ae65034bd648f15d124360fdbb0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-13 15:11:05 +0400

    refac(DateRange): use DATE_RANGE.map isntead of wtice using of InputDate component

--- src/features/order-table/components/DateRange.tsx
+++ src/features/order-table/components/DateRange.tsx
@@ -8,24 +8,23 @@ export default function DateRange() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
-
-  const fromDate = searchParams.get("fromDate") || "";
-  const toDate = searchParams.get("toDate") || "";
-
   const { updateDateParam } = useDateParams(pathname, router, searchParams);
+
+  const DATE_RANGE = [
+    { name: "fromDate" as const, value: searchParams.get("fromDate") || "" },
+    { name: "toDate" as const, value: searchParams.get("toDate") || "" },
+  ];
 
   return (
     <div className="flex gap-2">
-      <InputDate
-        name="fromDate"
-        value={fromDate}
-        handleChange={(e) => updateDateParam("fromDate", e.target.value)}
-      />
-      <InputDate
-        name="toDate"
-        value={toDate}
-        handleChange={(e) => updateDateParam("toDate", e.target.value)}
-      />
+      {DATE_RANGE.map(({ name, value }) => (
+        <InputDate
+          key={name}
+          name={name}
+          value={value}
+          handleChange={(e) => updateDateParam(name, e.target.value)}
+        />
+      ))}
     </div>
   );
 }
