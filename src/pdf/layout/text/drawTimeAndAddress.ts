9945fee06c215e900593a15518ed7ed2fe2f7a3c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-03 15:27:49 +0400

    feat(pdf): dinished placing time and address

--- src/pdf/layout/text/drawTimeAndAddress.ts
+++ src/pdf/layout/text/drawTimeAndAddress.ts
@@ -14,29 +14,33 @@ export const drawTimeAndAddress = ({
   x,
   y,
 }: DrawTimeAndAddress) => {
+  const initial_y = y;
+  y -= 30;
   drawer.drawText("დაწყების დრო:", x, y, {
     size: 9,
     bold: true,
   });
   drawer.drawText(formData.startTime, x + 80, y, { size: 9 });
-  y += 12;
+  y -= 12;
 
   drawer.drawText("დასრულების დრო:", x, y, {
     size: 9,
     bold: true,
   });
   drawer.drawText(formData.endTime, x + 100, y, { size: 9 });
-  y += 18;
+  y -= 18;
 
   drawer.drawText("ობიექტის მისამართი:", x, y, {
     size: 9,
     bold: true,
   });
-  y += 18;
+  y -= 18;
 
-  drawer.drawParagraph(formData.address, x, y, 170, {
+  drawer.drawParagraph(formData.address, x, y, 150, {
     size: 9,
     lineHeight: 1.3,
   });
-  return [100];
+  const usedHeight = initial_y - y;
+
+  return [usedHeight];
 };
