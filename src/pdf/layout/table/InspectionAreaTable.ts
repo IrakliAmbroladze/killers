c51c91ab2433b2b3005c1c5c0aafa9bed33f1fd6
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-21 21:58:46 +0400

    feat(pdfInspection): finished pdf table for kitchen area

--- src/pdf/layout/table/InspectionAreaTable.ts
+++ src/pdf/layout/table/InspectionAreaTable.ts
@@ -13,6 +13,16 @@ const inspectionOutdoor = [
   "შენობის გარშემო არ გროვდება ნაგავი      მოწესრიგებულია ბალახი და სხვა მცენარეები",
 ];
 
+const inspectionKitchen = [
+  "კედლები და ჭერი არის სუფთა, არ არის ცხიმიანი      და არ იქმნება კონდესატი",
+  "იატაკი და ტრაპები არის სუფთა, არ გროვდება ცხიმი ან ნარჩენები",
+  "საკვების მოსამზადებელი სივრცე მოწესრიგებულია, არ რჩება ნარჩენები",
+  "საკვები არ არის ხელმისაწვდომი მავნებლისთვის",
+  "საკვების მოსამზადებელი დანადგარები არის სუფთა, არ გროვდება ცხიმი ან ნარჩენები",
+  "ჭურჭლის სარეცხი სივრცე სუფთადაა      შენარჩუნებულია სიმშრალე",
+  "გასარეცხი ჭურჭელი არ გროვდება სამზარეულოში",
+];
+
 type drawInpectionAreaTableProps = {
   drawer: PDFDrawer;
   cursor: Cursor;
@@ -109,5 +119,66 @@ export const drawInpectionAreaTable = ({
     checkbox_X + 437,
     checkbox_Y - 135,
     getCheckboxState(formData.criteria["6.2"]),
+  );
+
+  drawer.drawTable(MARGIN_X, cursor.y - 145, {
+    headers: [
+      { text: "", width: FIRST_COLUMN_WIDTH, align: "center" },
+      { text: "სამზარეულო", width: SECOND_COLUMN_WIDTH, align: "center" },
+    ],
+    rows: inspectionKitchen.map((area, index) => [
+      { type: "text", text: String(index + 7), align: "center" },
+      {
+        type: "text",
+        text: area,
+        align: "left",
+      },
+    ]),
+  });
+  drawer.drawCheckbox(
+    checkbox_X + 253,
+    checkbox_Y - 180,
+    getCheckboxState(formData.criteria["7.1"]),
+  );
+
+  drawer.drawCheckbox(
+    checkbox_X + 390,
+    checkbox_Y - 180,
+    getCheckboxState(formData.criteria["7.2"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 335,
+    checkbox_Y - 200,
+    getCheckboxState(formData.criteria["8.1"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 370,
+    checkbox_Y - 220,
+    getCheckboxState(formData.criteria["9.1"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 260,
+    checkbox_Y - 240,
+    getCheckboxState(formData.criteria["10.1"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 437,
+    checkbox_Y - 260,
+    getCheckboxState(formData.criteria["11.1"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 196,
+    checkbox_Y - 280,
+    getCheckboxState(formData.criteria["12.1"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 345,
+    checkbox_Y - 280,
+    getCheckboxState(formData.criteria["12.2"]),
+  );
+  drawer.drawCheckbox(
+    checkbox_X + 260,
+    checkbox_Y - 300,
+    getCheckboxState(formData.criteria["13.1"]),
   );
 };
