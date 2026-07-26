33b395704f80178caa305e58b1b345ed5db41f58
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-26 08:50:42 +0400

    style(Column): add style for cards column

--- src/features/to-dos-board/Column.tsx
+++ src/features/to-dos-board/Column.tsx
@@ -5,7 +5,7 @@ type ColumnProps = Omit<ToDoColumn, "id">;
 
 export const Column = ({ title, taskList }: ColumnProps): ReactElement => {
   return (
-    <div>
+    <div className="flex flex-col w-80 shrink-0 bg-gray-100 dark:bg-gray-800 rounded-lg p-2.5 ">
       <h2>{title}</h2>
       {taskList.map(
         (task: Task): ReactElement => (
