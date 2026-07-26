90320159a05d6c89cfaf6097e4545ef60a75d65d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-26 09:06:27 +0400

    feat(ToDosBoard): add new Header component

--- src/features/to-dos-board/Column.tsx
+++ src/features/to-dos-board/Column.tsx
@@ -1,12 +1,17 @@
+"use client";
 import { ReactElement } from "react";
 import { Task, ToDoColumn } from "./types/ToDosBoardProps";
+import { Header } from "./Header";
 
 type ColumnProps = Omit<ToDoColumn, "id">;
 
 export const Column = ({ title, taskList }: ColumnProps): ReactElement => {
+  const handleClick = (): void => {
+    console.log("clicked");
+  };
   return (
     <div className="flex flex-col w-80 shrink-0 bg-gray-100 dark:bg-gray-800 rounded-lg p-2.5 ">
-      <h2>{title}</h2>
+      <Header status={title} onHandleClick={handleClick} />
       {taskList.map(
         (task: Task): ReactElement => (
           <div key={task.id}>{task.node}</div>
---
+++ src/features/to-dos-board/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Header = ({
+  status,
+  onHandleClick,
+}: {
+  status: string;
+  onHandleClick: (e: React.MouseEvent) => void;
+}): React.ReactElement => {
+  return (
+    <button
+      onClick={onHandleClick}
+      className="bg-green-500 text-white text-xs rounded-t-lg justify-center text-center p-1.5 cursor-pointer active:scale-95 ease-in-out transition-transform duration-150 hover:scale-102"
+    >
+      {status}
+    </button>
+  );
+};
