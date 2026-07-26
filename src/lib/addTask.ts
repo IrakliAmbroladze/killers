430105a6226a3a2a192bd4a2c50090c605415e1b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-27 00:55:11 +0400

    feat(addTask): adds author on creating a new task

--- src/lib/addTask.ts
+++ src/lib/addTask.ts
@@ -11,9 +11,17 @@ export const addTask = async ({
   description: string;
   column_id: number;
 }) => {
-  const newTask = { title, description, column_id };
-
   const supabase = await createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const newTask = {
+    title,
+    description,
+    column_id,
+    author_id: session?.user.id,
+  };
   const { error } = await supabase.from("teams_tasks").insert([newTask]);
 
   if (error) {
