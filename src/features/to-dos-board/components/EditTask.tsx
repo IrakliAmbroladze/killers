f3db47acd22dd9796379da7e075993d417507e52
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-08-12 07:57:16 +0400

    feat(EditTask): add coomments section

--- src/features/to-dos-board/components/EditTask.tsx
+++ src/features/to-dos-board/components/EditTask.tsx
@@ -1,6 +1,7 @@
 // src/features/to-dos-board/EditTask.tsx
 import { FormEvent, useEffect, useState } from "react";
 import { getTask } from "@/lib/getTask";
+import Comments from "@/features/comments/components/comments";
 
 type EditTaskProps = {
   id: string | null;
@@ -69,78 +70,81 @@ export const EditTask = ({
   }
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-5">
-      <div>
-        <label
-          htmlFor="task-title"
-          className="mb-2 block text-sm font-medium text-gray-700"
-        >
-          Title
-        </label>
-        <input
-          id="task-title"
-          type="text"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-          placeholder="Enter task title"
-          className="w-full rounded-lg border border-gray-300 px-3 py-2 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
-        />
-      </div>
+    <div className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="space-y-5">
+        <div>
+          <label
+            htmlFor="task-title"
+            className="mb-2 block text-sm font-medium text-gray-700"
+          >
+            Title
+          </label>
+          <input
+            id="task-title"
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="Enter task title"
+            className="w-full rounded-lg border border-gray-300 px-3 py-2 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
+          />
+        </div>
 
-      <div>
-        <label
-          htmlFor="task-description"
-          className="mb-2 block text-sm font-medium text-gray-700"
-        >
-          Description
-        </label>
-        <textarea
-          id="task-description"
-          rows={4}
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          placeholder="Enter task description"
-          className="w-full rounded-lg border border-gray-300 px-3 py-2 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
-        />
-      </div>
-      <div className="flex justify-between">
-        <button
-          type="button"
-          onClick={() => onDelete(id)}
-          className="rounded-lg px-4 py-2 text-sm font-medium text-red-700 dark:text-red-300 transition hover:underline cursor-pointer active:scale-95 ease-in-out duration-150"
-        >
-          delete
-        </button>
-        <select
-          name="todolist-status"
-          id="todolist-status"
-          className="dark:bg-blue-900 bg-gray-400 rounded-lg"
-          value={statusId}
-          onChange={(e) => {
-            setStatusId(Number(e?.target?.value));
-          }}
-        >
-          <option value={0}>To do</option>
-          <option value={1}>In progress</option>
-          <option value={2}>Done</option>
-        </select>{" "}
-        <div className="flex justify-end gap-3">
+        <div>
+          <label
+            htmlFor="task-description"
+            className="mb-2 block text-sm font-medium text-gray-700"
+          >
+            Description
+          </label>
+          <textarea
+            id="task-description"
+            rows={4}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            placeholder="Enter task description"
+            className="w-full rounded-lg border border-gray-300 px-3 py-2 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
+          />
+        </div>
+        <div className="flex justify-between">
           <button
             type="button"
-            onClick={onCancel}
-            className="rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium transition hover:bg-gray-100"
+            onClick={() => onDelete(id)}
+            className="rounded-lg px-4 py-2 text-sm font-medium text-red-700 dark:text-red-300 transition hover:underline cursor-pointer active:scale-95 ease-in-out duration-150"
           >
-            Cancel
+            delete
           </button>
-          <button
-            type="submit"
-            disabled={!title.trim()}
-            className="rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:opacity-50"
+          <select
+            name="todolist-status"
+            id="todolist-status"
+            className="dark:bg-blue-900 bg-gray-400 rounded-lg"
+            value={statusId}
+            onChange={(e) => {
+              setStatusId(Number(e?.target?.value));
+            }}
           >
-            Save changes
-          </button>
+            <option value={0}>To do</option>
+            <option value={1}>In progress</option>
+            <option value={2}>Done</option>
+          </select>{" "}
+          <div className="flex justify-end gap-3">
+            <button
+              type="button"
+              onClick={onCancel}
+              className="rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium transition hover:bg-gray-100"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              disabled={!title.trim()}
+              className="rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:opacity-50"
+            >
+              Save changes
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+      <Comments id={id} />
+    </div>
   );
 };
