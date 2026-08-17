8dd6409eff0fc58018951e97f87c2265dec3ed75
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-08-17 10:04:07 +0400

    refactor(AcceptanceDocument): move stamp down

--- src/pdf/builders/acceptanceBuilder.ts
+++ src/pdf/builders/acceptanceBuilder.ts
@@ -155,7 +155,7 @@ export async function buildAcceptancePdf(formData: AcceptanceFormData) {
     pdf,
     pageNumber: 2,
   });
-  secondDrawer.drawImage(stampImage, PAGE_WIDTH / 2 - 40, secondCursor.y - 20, {
+  secondDrawer.drawImage(stampImage, PAGE_WIDTH / 2 - 40, secondCursor.y - 60, {
     height: 80,
   });
   const pdfBytes = await pdf.save();
